6822eadac4e0704bc50b9dbd3b26c4eb7496b26a
usman699 <musmanguzar93@gmail.com>
2024-12-06 17:02:01 +0500

    addded wallapers

---
+++ NoteKeeper/login/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  NoteKeeper
+//
+//  Created by Spark Solutionz Inc on 06/12/2024.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
---
+++ NoteKeeper/utils/Textfeild.swift
@@ -0,0 +1,18 @@
+//
+//  Textfeild.swift
+//  NoteKeeper
+//
+//  Created by Spark Solutionz Inc on 06/12/2024.
+//
+
+import SwiftUI
+
+struct Textfeild: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Textfeild()
+}
