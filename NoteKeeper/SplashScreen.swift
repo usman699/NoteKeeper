e94640c76d13e9d600b3cdfbd6d338b0469877f0
usman699 <musmanguzar93@gmail.com>
2024-12-06 11:04:38 +0500

    new commit

---
+++ NoteKeeper/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  NoteKeeper
+//
+//  Created by Spark Solutionz Inc on 05/12/2024.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
